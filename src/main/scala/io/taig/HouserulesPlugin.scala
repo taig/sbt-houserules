5e22468ca99aba2991ff0dc862c6e74999f1904c
Niklas Klein <mail@taig.io>
2023-09-08 12:03:27 +0200

    Upgrade to scalafmt 3.7.14

--- src/main/scala/io/taig/HouserulesPlugin.scala
+++ src/main/scala/io/taig/HouserulesPlugin.scala
@@ -46,7 +46,7 @@ object HouserulesPlugin extends AutoPlugin {
       "maxColumn" -> "120",
       "rewrite.rules" -> "[Imports, SortModifiers]",
       "rewrite.imports.sort" -> "original",
-      "version" -> "3.7.7",
+      "version" -> "3.7.14",
       "runner.dialect" -> (CrossVersion.partialVersion(scalaVersion.value) match {
         case Some((2, 11)) => "scala211"
         case Some((2, 12)) => "scala212"
