c1b074906063304a1ff5fe1933ebf231df15c220
Niklas Klein <mail@taig.io>
2025-01-13 15:41:38 +0100

    Upgrade to scalafmt 3.8.4

--- src/main/scala/io/taig/HouserulesScalafmtPlugin.scala
+++ src/main/scala/io/taig/HouserulesScalafmtPlugin.scala
@@ -21,7 +21,7 @@ object HouserulesScalafmtPlugin extends AutoPlugin {
 
   override def globalSettings: Seq[Def.Setting[_]] = Def.settings(
     scalafmtConfiguration := ListMap(
-      "version" -> "3.8.3",
+      "version" -> "3.8.4",
       "maxColumn" -> "120",
       "assumeStandardLibraryStripMargin" -> "true",
       "rewrite.rules" -> "[Imports, SortModifiers]",
